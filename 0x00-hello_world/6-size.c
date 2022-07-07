ab043463dfa7ce1dbdddb0f5c73c1aa18e1815a0
ttytgfjhgt <sannypeter1@gmail.com>
2022-07-07 14:09:43 -0700

    Task 0

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,29 +3,17 @@
 
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
 
-		printf("Size of a char: %ld byte(s)\n", sizeof(char));
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 
-			printf("Size of an int: %ld byte(s)\n", sizeof(int));
-
-				printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-
-					printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-
-						printf("Size of a float: %ld byte(s)\n", sizeof(float));
-
-							return (0);
+			return (0);
 
 }
