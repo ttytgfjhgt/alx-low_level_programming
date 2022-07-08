d4428c51777a1f6a9381a3c911207b7c666968d3
ttytgfjhgt <sannypeter1@gmail.com>
2022-07-08 16:58:09 -0700

    functions and nested loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+
+
+
+
+
+
+/**
+ * print_triangle - print a triangle pattern
+ * @size: l
+ * Return: void
+ */
+
+
+
+void print_triangle(int size)
+
+{
+
+	int i, j, x;
+
+	if (size <= 0)
+
+	{
+
+		_putchar('\n');
+
+		return;
+
+	}
+
+	for (i = 1; i <= size; i++)
+
+	{
+
+		for (j = 1; j <= size; j++)
+
+		{
+
+			x = j < (size + 1) - i ? ' ' : '#';
+
+			_putchar(x);
+		}
+
+		_putchar('\n');
+	}
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+
+
+
+
+
+
+/**
+ * print_square - print a square pattern
+ * @size: l
+ * Return: void
+ */
+
+
+
+void print_square(int size)
+
+{
+
+	int i, j;
+
+	if (size <= 0)
+
+	{
+
+		_putchar('\n');
+
+		return;
+
+	}
+
+	for (i = 0; i < size; i++)
+
+	{
+
+		for (j = 0; j < size; j++)
+
+		{
+
+			_putchar('#');
+
+		}
+
+		_putchar('\n');
+
+	}
+}
