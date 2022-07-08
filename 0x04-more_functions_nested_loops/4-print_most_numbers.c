97691456253197d9ead30d8145dfaa00e1194c5b
ttytgfjhgt <sannypeter1@gmail.com>
2022-07-08 16:32:15 -0700

    functions and nested loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+
+
+/**
+ * print_most_numbers - mai description
+ *
+ * return: numbers and skip 2 and 4
+ */
+
+
+
+void print_most_numbers(void)
+
+{
+
+	int i;
+
+
+
+	for (i = '0'; i <= '9'; i++)
+
+	{
+
+		if (i == '2' || i == '4')
+
+		{
+
+			continue;
+
+		}
+
+		_putchar(i);
+
+	}
+
+	_putchar('\n');
+
+}
