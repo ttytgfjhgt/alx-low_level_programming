e9a795ac63718a31c2716a70b322e9ca80dedc83
ttytgfjhgt <sannypeter1@gmail.com>
2022-07-08 16:19:09 -0700

    functions and nested loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+
+/**
+ * _isdigit - main description
+ * @c: return a digit
+ * Return: 1 is success 0 failure
+ */
+
+
+
+int _isdigit(int c)
+
+{
+
+	if (c >= '0' && c <= '9')
+
+	{
+
+		return (1);
+
+	}
+
+	else
+
+		return (0);
+
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 
 #define MAIN_H
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 
 
