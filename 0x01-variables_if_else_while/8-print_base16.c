8344b9d19cb7e7addd39abdf67838f7b55497f79
ttytgfjhgt <sannypeter1@gmail.com>
2022-07-08 14:14:29 -0700

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,37 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+
+int main(void)
+
+{
+
+int i;
+
+
+
+for (i = 48 ; i < 58 ; i++)
+
+{
+
+putchar(i);
+
+}
+
+for (i = 97 ; i < 103 ; i++)
+
+{
+
+putchar(i);
+
+}
+
+putchar('\n');
+
+return (0);
+
+}
