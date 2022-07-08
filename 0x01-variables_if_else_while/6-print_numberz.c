682a8a1c65f4211adc386cd46c450172c0922226
ttytgfjhgt <sannypeter1@gmail.com>
2022-07-08 14:09:20 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+
+int main(void)
+
+{
+
+int i = 0;
+
+for (i = 48; i <= 57; i++)
+
+{
+
+putchar(i);
+
+}
+
+putchar('\n');
+
+return (0);
+
+}
