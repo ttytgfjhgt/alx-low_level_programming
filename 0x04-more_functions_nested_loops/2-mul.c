14ec6ef9425e03e0f86e23f6e4dab18a5f5146a9
ttytgfjhgt <sannypeter1@gmail.com>
2022-07-08 16:27:25 -0700

    functions and nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+
+
+
+/**
+ * mul - multiply two integers
+ *@a: integer
+ *@b: integer
+ * Return: reurn the multiple
+ */
+
+
+
+int mul(int a, int b)
+
+{
+
+	return (a * b);
+
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+
+/**
+ * print_numbers - print numbers
+ *
+ * Return: numbers and a new line
+ */
+
+
+
+void print_numbers(void)
+
+{
+
+	int i;
+
+
+
+	for (i = '0'; i <= '9'; i++)
+
+	{
+
+		_putchar(i);
+
+	}
+
+	_putchar('\n');
+
+}
