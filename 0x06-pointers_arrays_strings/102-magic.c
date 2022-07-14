2c7218adab91793efa327f2325849a5b16823246
ttytgfjhgt <sannypeter1@gmail.com>
2022-07-14 01:08:47 -0700

    pointers arrays and strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,37 +1,25 @@
-#include "main.h"
+#include <stdio.h>
 
 
-/**
- * print_number - print an integer, without using long, arrays, or pointers
- * @n: number to be printed
- */
-
-void print_number(int n)
+int main(void)
 
 {
-	unsigned int tens, dig, pos = n;
-	double temp_tens = 1;
+  int n;
+  int a[5];
+  int *p;
 
-	if (n == 0)
-		_putchar('0');
-	else
-	{
-		if (n < 0)
-		{
-			pos = n * -1;
-			_putchar('-');
-		}
-
-		while (temp_tens <= pos)
-			temp_tens *= 10;
-		tens = temp_tens / 10;
-
-		while (tens >= 1)
-		{
-			dig = pos / tens;
-			_putchar(dig + '0');
-			pos = (pos - (tens * dig));
-			tens /= 10;
-		}
-	}
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
